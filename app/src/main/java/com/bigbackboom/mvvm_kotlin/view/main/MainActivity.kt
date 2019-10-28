d35c5470d1fd9ef586bee2a21285d47708d70abb
bigbackboom <k.kikuchi401@cb-cloud.com>
2019-10-29 08:23:18 +0900

    不要のものの調整

--- app/src/main/java/com/bigbackboom/mvvm_kotlin/view/main/MainActivity.kt
+++ app/src/main/java/com/bigbackboom/mvvm_kotlin/view/main/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.bigbackboom.mvvm_kotlin.view.main
 
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProviders
 import com.bigbackboom.mvvm_kotlin.R
-import com.bigbackboom.mvvm_kotlin.di.ViewModelFactory
 import com.bigbackboom.mvvm_kotlin.view.manager.FragmentManager
-import com.bigbackboom.mvvm_kotlin.viewmodel.MainViewModel
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
